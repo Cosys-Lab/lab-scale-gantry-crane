126e4ac39e6296e44e5d573a003c29280c98ad3d
Joost Mertens <joost.mertens@uantwerpen.be>
2025-05-06 17:51:33 +0200

    Add database initialization scripts

---
+++ database/populate-machine.sql
@@ -0,0 +1,18 @@
+INSERT INTO public.machine
+(machine_id, "name")
+VALUES(2, 'group 2');
+INSERT INTO public.machine
+(machine_id, "name")
+VALUES(3, 'group 3');
+INSERT INTO public.machine
+(machine_id, "name")
+VALUES(4, 'group 4');
+INSERT INTO public.machine
+(machine_id, "name")
+VALUES(5, 'group 5');
+INSERT INTO public.machine
+(machine_id, "name")
+VALUES(6, 'group 6');
+INSERT INTO public.machine
+(machine_id, "name")
+VALUES(1, 'group 1');
