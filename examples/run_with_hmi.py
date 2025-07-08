debc055b17320bd5f49fd74b446edbd7b8b51a66
Joost Mertens <joost.mertens@uantwerpen.be>
2025-07-08 15:54:40 +0200

    Update repo readme

--- examples/run_with_hmi.py
+++ examples/run_with_hmi.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-from unittest.mock import MagicMock
-from gantrylib.gantry_controller import MockGantryController
+from gantrylib.gantry_controller import PhysicalGantryController
 from gantrylib.hmi import MotionGUI
 import yaml
 
@@ -9,9 +8,7 @@ if __name__ == "__main__":
     root = tk.Tk()
     with open("./crane-properties.yaml", "r") as file:
         crane_properties = yaml.safe_load(file)
-        with MockGantryController(crane_properties) as crane_controller:
-            mock_crane = MagicMock()
-            mock_crane.getState = MagicMock(return_value=(0, 0, 0, 0, 0, 0, 0))
-            crane_controller.crane = mock_crane
-            app = MotionGUI(root, crane_controller)
+        with PhysicalGantryController(crane_properties) as crane_controller:
+            cfg = yaml.safe_load(open("../examples/crane-properties.yaml"))
+            app = MotionGUI(root, crane_controller, cfg)
             root.mainloop()
